6f88c5ac0e20867f17b194787f1a3e9b5bcd74e8
Jaennie <je028770@gmail.com>
2020-04-18 18:59:11 +0900

    [2020-04-18 18:58] create VO

---
+++ src/main/java/com/ceres/minee/vo/GalleryItem.java
@@ -0,0 +1,17 @@
+package com.ceres.minee.vo;
+
+public class GalleryItem extends ResultItem{
+	
+	private String imgUrl;
+	
+	public GalleryItem(String title, String url,String imgUrl) {
+		super(title, url);
+		this.imgUrl = imgUrl;
+	}
+
+	public String getImgUrl() {return imgUrl;}
+
+	public void setImgUrl(String imgUrl) {this.imgUrl = imgUrl;}
+	
+	
+}
