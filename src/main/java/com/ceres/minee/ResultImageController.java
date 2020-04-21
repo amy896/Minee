c1a5f7f9693fcac8c4fec45c6bf18b52c34a611e
Shin <smy11go@gmail.com>
2020-04-21 14:58:00 +0900

    [2020-04-21 14:57] ResultImageController 작업 중

--- src/main/java/com/ceres/minee/ResultImageController.java
+++ src/main/java/com/ceres/minee/ResultImageController.java
@@ -1,5 +1,7 @@
 package com.ceres.minee;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ResultImageController {
 	
+	public void tempSetFianlResults() {
+		
+	}
+	
 	@RequestMapping(value = "resultImage", method = RequestMethod.GET)
 	public void resultImage() {
+		ArrayList<String> finalResults = new ArrayList();
+		finalResults.add("Hi,");
+		finalResults.add("Hello~");
+		finalResults.add("LOL");
+	}
+	
+	public boolean nextPage(int pageNumber) {
+		return true;
 	}
 	
 }
